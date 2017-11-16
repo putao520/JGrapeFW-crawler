02d0320e9fb24fd15de90270aa79b556fc221619
yuyao <yuyao@DESKTOP-UMO8UO5>
2017-11-16 08:07:49 +0800

    1112

--- src/main/java/interfaceApplication/task.java
+++ src/main/java/interfaceApplication/task.java
@@ -228,7 +228,7 @@ public class task {
 							urlContent nextUrlObj;
 							switch(numberHelper.number2int(loopMode) ) {
 							case 1://通过起始页获得
-								nextUrlObj = getURL( "", contentUrlObj.getUp(), loopJson.getString("selecter") );//获得下一页URL
+								nextUrlObj = getURL( "", contentURL, loopJson.getString("selecter") );//获得下一页URL
 								loopURL = nextUrlObj != null ? nextUrlObj.getCur() : null;
 								break;
 							case 2://通过内容页获得
